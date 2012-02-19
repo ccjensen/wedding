9d31c55c56ed10ca3d6ee275edf98161653cab1f
Christopher Jensen <christophercjensen@gmail.com>
2012-02-19 13:51:07 -0800

    tweaked theme, added sunflower

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/wedding/"
+http_images_path = "/wedding/images"
+http_fonts_path = "/wedding/fonts"
+css_dir = "public/wedding/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
